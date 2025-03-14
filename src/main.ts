24def5460b0100786ae40bdf21b69434db5ed7e7
Haseeb S <haseeb.sultan420@gmail.com>
2025-03-14 20:43:11 +0500

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*', 
+    // Allow specific origin methods: 'GET,POST', 
+    // Allow specific methods allowedHeaders: 'Content-Type, Authorization', 
+    // Allow specific headers 
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
